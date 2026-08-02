ee278341e125e64db0c8a4a4887ba8bc980439e0
hanlihanshaobo <qingqingruwushaobohan@gmail.com>
2026-08-03 01:51:22 +0800

    Update feeds and add AX1800Pro build

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,12 +8,6 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.c
 # cupsd
 sed -i '$a src-git cups https://github.com/sirpdboy/luci-app-cupsd.git' ./feeds.conf.default
 # 集客AC
-sed -i '$a src-git gecoosac https://github.com/lwb1978/openwrt-gecoosac.git' ./feeds.conf.default
+sed -i '$a src-git gecoosac https://github.com/bleach1991/openwrt-gecoosac.git' ./feeds.conf.default
 # 应用过滤
 sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' ./feeds.conf.default
-# openclash
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' ./feeds.conf.default
-# luci
-sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' ./feeds.conf.default
-# 注释luci
-sed -i '3 s/^/#/' ./feeds.conf.default
