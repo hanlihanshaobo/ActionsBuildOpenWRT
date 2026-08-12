444f6b6acf3842915ce01a7946a1435437589b04
hanlihanshaobo <qingqingruwushaobohan@gmail.com>
2026-08-12 20:44:56 +0800

    chore: 注释 diy-part1/2 全部自定义，使用 lede 默认配置构建 ax1800pro

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,14 +4,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # ssr-plus
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 # cupsd
-sed -i '$a src-git cups https://github.com/sirpdboy/luci-app-cupsd.git' ./feeds.conf.default
+#sed -i '$a src-git cups https://github.com/sirpdboy/luci-app-cupsd.git' ./feeds.conf.default
 # 集客AC
-sed -i '$a src-git gecoosac https://github.com/bleach1991/openwrt-gecoosac.git' ./feeds.conf.default
+#sed -i '$a src-git gecoosac https://github.com/bleach1991/openwrt-gecoosac.git' ./feeds.conf.default
 # 应用过滤
-sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' ./feeds.conf.default
+#sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' ./feeds.conf.default
 # USB移动数据网络
-sed -i '$a src-git usbmodem https://github.com/1391959853/luci-app-usbmodem.git' ./feeds.conf.default
+#sed -i '$a src-git usbmodem https://github.com/1391959853/luci-app-usbmodem.git' ./feeds.conf.default
 # eMMC健康检测/分区管理
-sed -i '$a src-git routetool https://github.com/rothdren-lion/luci-app-route-tool.git' ./feeds.conf.default
+#sed -i '$a src-git routetool https://github.com/rothdren-lion/luci-app-route-tool.git' ./feeds.conf.default
